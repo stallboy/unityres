4235429f5f1501817f8bf3dd04af663a74bbecd2
cheng xiaosan <stallboy@qq.com>
2016-12-07 13:18:53 +0800

    simple event dump

--- res/resdumper.lua
+++ res/resdumper.lua
@@ -1,28 +1,24 @@
 local Cache = require "res.Cache"
 local Pool = require "res.Pool"
-local Timer = require "common.timer"
+local Timer = require "common.Timer"
+local SimpleEvent = require "common.SimpleEvent"
 
 local resdumper = {}
 
+local function _dump(mod)
+    local t = {}
+    mod.dumpAll(function(info)
+        table.insert(t, info)
+    end)
+    UnityEngine.Debug.Log(table.concat(t, "\n"))
+end
+
 function resdumper.dump()
     ResUpdater.ResDumper.Dump();
-    local t = {}
-    Pool.dumpAll(function(info)
-        table.insert(t, info)
-    end)
-    UnityEngine.Debug.Log(table.concat(t, "\n"))
-
-    local t = {}
-    Cache.dumpAll(function(info)
-        table.insert(t, info)
-    end)
-    UnityEngine.Debug.Log(table.concat(t, "\n"))
-
-    local t = {}
-    Timer.dumpAll(function(info)
-        table.insert(t, info)
-    end)
-    UnityEngine.Debug.Log(table.concat(t, "\n"))
+    _dump(Pool)
+    _dump(Cache)
+    _dump(Timer)
+    _dump(SimpleEvent)
 end
 
 return resdumper
