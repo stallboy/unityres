743b4637182735de24f05dade5a3aebd44e2245c
stallboy <chengxiaosan@gmail.com>
2020-04-21 16:01:57 +0800

    打包策略

---
+++ res/ABAssetInfos.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Config;
+
+namespace BuildSystem
+{
+    public class ABAssetinfos
+    {
+        public class AssetInfo
+        {
+            public string asset;
+
+            public bool isMarked;
+            public bool isNeedMark;
+
+            public int memSize;
+            public int canSaveMemSize;
+            public int containingABCount;
+
+            public HashSet<string> containingABs = new HashSet<string>();
+            public HashSet<string> directContainingAssets = new HashSet<string>();
+        }
+
+        private class Comparer : IComparer<AssetInfo>
+        {
+            public int Compare(AssetInfo x, AssetInfo y)
+            {
+                int save = y.canSaveMemSize - x.canSaveMemSize;
+                if (save != 0)
+                {
+                    return save;
+                }
+
+                int mem = y.memSize - x.memSize;
+                if (mem != 0)
+                {
+                    return mem;
+                }
+
+                return string.Compare(x.asset, y.asset, StringComparison.Ordinal);
+            }
+        }
+
+        public static IComparer<AssetInfo> assetinfoCmp = new Comparer();
+
+        public List<AssetInfo> sortedAllAssetInfos = new List<AssetInfo>();
+
+        public Dictionary<string, AssetInfo> allAssetInfoMap = new Dictionary<string, AssetInfo>();
+        public long canSaveSum;
+        public long allSize;
+        public string canSavePercent;
+        public string sumStr;
+
+        public void Add(AssetInfo ai)
+        {
+            sortedAllAssetInfos.Add(ai);
+            allAssetInfoMap.Add(ai.asset, ai);
+        }
+
+        public bool Get(string path, out AssetInfo ai)
+        {
+            return allAssetInfoMap.TryGetValue(path, out ai);
+        }
+
+        public void Sort()
+        {
+            sortedAllAssetInfos.Sort(assetinfoCmp);
+        }
+
+        public void Sum()
+        {
+            canSaveSum = 0;
+            allSize = 0;
+            foreach (var ai in sortedAllAssetInfos)
+            {
+                canSaveSum = canSaveSum + ai.canSaveMemSize;
+                allSize = allSize + ai.memSize*ai.containingABCount;
+                //Debug.Log(ai.asset + "," + ai.canSaveMemSize + "," + ai.memSize + "," + ai.containingABCount + "," + canSaveSum + "," + allSize);
+            }
+            double percent = (double) canSaveSum/allSize;
+            canSavePercent = percent.ToString("0.00");
+            sumStr = "cnt=" + sortedAllAssetInfos.Count + "," +
+                     "cansave/all=" + ToolUtils.readableSize(canSaveSum) + "/" +
+                     ToolUtils.readableSize(allSize) + "=" +
+                     canSavePercent;
+        }
+
+        public void SaveToCsv(string saveFn)
+        {
+            using (var texter = new StreamWriter(saveFn, false)) //no bom
+            {
+                var header1 = new[]
+                {
+                    "asset",
+                    "isMarked",
+                    "allMem=" + ToolUtils.readableSize(allSize),
+                    "canSave=" + ToolUtils.readableSize(canSaveSum),
+                    "percent=" + canSavePercent,
+                    "count=" + sortedAllAssetInfos.Count,
+                    ""
+                };
+
+                texter.WriteLine(string.Join(",", header1));
+
+                var header2 = new[]
+                {
+                    "asset", "isMarked", "memSize", "canSaveMemSize", "count", "containingABs", "directContainingAssets"
+                };
+                texter.WriteLine(string.Join(",", header2));
+
+                foreach (var ai in sortedAllAssetInfos)
+                {
+                    var line = new[]
+                    {
+                        ai.asset,
+                        ai.isMarked ? "1" : "0",
+                        ai.memSize.ToString(),
+                        ai.canSaveMemSize.ToString(),
+                        ai.containingABCount.ToString(),
+                        string.Join(":", ai.containingABs.ToArray()),
+                        string.Join(":", ai.directContainingAssets.ToArray()),
+                    };
+
+                    texter.WriteLine(string.Join(",", line));
+                }
+            }
+        }
+
+        public void LoadFromCsv(string saveFn)
+        {
+            sortedAllAssetInfos.Clear();
+            allAssetInfoMap.Clear();
+            var lines = CSV.Parse(new StreamReader(saveFn));
+            for (int i = 2; i < lines.Count; i++)
+            {
+                var line = lines[i];
+                var ai = new AssetInfo
+                {
+                    asset = line[0],
+                    isMarked = line[1].Equals("1"),
+                    memSize = int.Parse(line[2]),
+                    canSaveMemSize = int.Parse(line[3]),
+                    containingABCount = int.Parse(line[4]),
+                    containingABs = new HashSet<string>(line[5].Split(':')),
+                    directContainingAssets = new HashSet<string>(line[6].Split(':'))
+                };
+                Add(ai);
+            }
+            Sum();
+        }
+    }
+}
---
+++ res/ABMarkDupChecker.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace BuildSystem
+{
+    public class ABMarkDupChecker
+    {
+        private static Dictionary<string, string> _markedAsset2Bundle;
+        private static Dictionary<string, ABAssetinfos.AssetInfo> _allAssetInfos;
+        private static ABAssetinfos _assetInfos;
+
+        private static bool IsMarked(string asset)
+        {
+            return _markedAsset2Bundle.ContainsKey(asset);
+        }
+
+        //这里的细节是得用直接依赖AssetDatabase.GetDependencies(thisAsset, false))，来计算重复率，不能使用全部依赖，
+        //比如a，b，c 3个asset，a包含b，b包含c。。
+        //a，b打成了A，B2个Bundle，那么c这个asset，的containingABs只能包含B，不能包含A。
+        private static void CollectUnmarkedAsset(string thisAsset, string bundle)
+        {
+            foreach (var asset in AssetDatabase.GetDependencies(thisAsset, false))
+            {
+                if (!asset.EndsWith(".cs") && !IsMarked(asset))
+                {
+                    ABAssetinfos.AssetInfo res;
+                    if (_allAssetInfos.TryGetValue(asset, out res))
+                    {
+                        res.containingABs.Add(bundle);
+                        res.directContainingAssets.Add(thisAsset);
+                    }
+                    else
+                    {
+                        res = new ABAssetinfos.AssetInfo {asset = asset, isMarked = false};
+                        res.containingABs.Add(bundle);
+                        res.directContainingAssets.Add(thisAsset);
+                        _allAssetInfos.Add(asset, res);
+                    }
+                    CollectUnmarkedAsset(asset, bundle);
+                }
+            }
+        }
+
+        private static long CalcSize(string asset)
+        {
+            if (asset.EndsWith(".unity"))
+            {
+                return 0;
+            }
+            var objs = AssetDatabase.LoadAllAssetsAtPath(asset);
+            long allsize = 0;
+            foreach (var obj in objs)
+            {
+                if (obj != null)
+                {
+                    var size = UnityEngine.Profiling.Profiler.GetRuntimeMemorySizeLong(obj);
+                    allsize += size;
+
+                    if (obj is GameObject || obj is Component)
+                    {
+                    }
+                    else
+                    {
+                        Resources.UnloadAsset(obj);
+                    }
+                }
+                else
+                {
+                    Debug.LogError(asset + " load=null");
+                }
+            }
+            return allsize;
+        }
+
+
+        public static void CheckDuplicate(string dupcsvfn)
+        {
+            _markedAsset2Bundle = new Dictionary<string, string>();
+            foreach (var bundle in AssetDatabase.GetAllAssetBundleNames())
+            {
+                foreach (var asset in AssetDatabase.GetAssetPathsFromAssetBundle(bundle))
+                {
+                    _markedAsset2Bundle.Add(asset, bundle);
+                }
+            }
+
+            Debug.Log("marked asset count=" + _markedAsset2Bundle.Count);
+            _allAssetInfos = new Dictionary<string, ABAssetinfos.AssetInfo>();
+            foreach (var kv in _markedAsset2Bundle)
+            {
+                CollectUnmarkedAsset(kv.Key, kv.Value);
+            }
+
+            foreach (var kv in _markedAsset2Bundle)
+            {
+                var ai = new ABAssetinfos.AssetInfo {asset = kv.Key, isMarked = true};
+                ai.containingABs.Add(kv.Value);
+                _allAssetInfos.Add(kv.Key, ai);
+            }
+            Debug.Log("all asset count=" + _allAssetInfos.Count);
+
+
+            _assetInfos = new ABAssetinfos();
+            foreach (var kv in _allAssetInfos)
+            {
+                var ai = kv.Value;
+                ai.memSize = (int) CalcSize(kv.Key);
+                ai.containingABCount = ai.containingABs.Count;
+                ai.canSaveMemSize = ai.memSize*(ai.containingABCount - 1);
+                _assetInfos.Add(ai);
+            }
+
+            _assetInfos.Sort();
+            _assetInfos.Sum();
+
+            Resources.UnloadUnusedAssets();
+
+
+            Debug.Log(_assetInfos.sumStr);
+
+            _assetInfos.SaveToCsv(dupcsvfn);
+            Debug.Log("save to " + dupcsvfn);
+        }
+    }
+}
